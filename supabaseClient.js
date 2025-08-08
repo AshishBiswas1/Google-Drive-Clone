aa14dea7126c620700518bfea6edf694c237cae6
Ashish Biswas <biswasashish655@gmail.com>
2025-08-08 09:54:08 +0530

    Initial Commit.

---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+const { createClient } = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+module.exports = supabase;
