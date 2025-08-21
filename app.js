93ba95736a7a07510b71a7b2083c798210b5f483
Ashish Biswas <biswasashish655@gmail.com>
2025-08-21 15:54:53 +0530

    New Completed Project.

--- app.js
+++ app.js
@@ -13,14 +13,28 @@ const AppError = require('./util/appError');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(
   cors({
-    origin: 'google-drive-clone-frontend-smoky.vercel.app',
+    origin: 'https://google-drive-clone-frontend-smoky.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
+
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', FRONTEND_ORIGIN);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+  );
+  return res.sendStatus(204);
+});
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
